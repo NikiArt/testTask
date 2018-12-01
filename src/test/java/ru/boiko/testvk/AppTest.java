6dcf50f6a864365decdd2522af2aa224938b1290
NikiArt <genertyniki@gmail.com>
2018-12-01 19:04:34 +0300

    working VkApi class. Clear old tests

--- src/test/java/ru/boiko/testvk/AppTest.java
+++ src/test/java/ru/boiko/testvk/AppTest.java
@@ -1,22 +1,7 @@
 package ru.boiko.testvk;
 
 import lombok.SneakyThrows;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.junit.Test;
-
-import java.awt.*;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Iterator;
 
 import static org.junit.Assert.assertTrue;
 
